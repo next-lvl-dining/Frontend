2b0cbb733f149e372718b8955493ef6a71e15014
Unknown <ruudvdboomen@hotmail.com>
2019-04-02 14:45:28 +0200

    Add config file

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,22 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Config } from '../config';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  API_URL = 'http://localhost:8080/login/api'
-
   constructor(private http: HttpClient) { }
 
   registerUser(user: any): Observable<Object> {
-    return this.http.post(this.API_URL + '/user', user)
+    return this.http.post(Config.API_URL + '/user', user)
       .pipe(catchError(this.errorHandler));
   }
 
   findUserByEmail(email: String): Observable<Object> {
-    return this.http.get(this.API_URL + '/user/email/' + email)
+    return this.http.get(Config.API_URL + '/user/email/' + email)
       .pipe(catchError(this.errorHandler));
   }
 
