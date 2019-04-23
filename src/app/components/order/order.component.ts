9386d78553dfa590bd3767723a03461041c142b7
Richard Delsink <rd.richard@hotmail.com>
2019-04-23 11:33:16 +0200

    Update: cart

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product/product.service';
+import {Item} from '../../models/item';
 
 @Component({
   selector: 'app-order',
@@ -11,14 +12,54 @@ import { ProductService } from '../../services/product/product.service';
 export class OrderComponent implements OnInit {
 
   private products: Product[];
-
+  private items: Item[] = [];
+  private total: number = 0;
   constructor(
     private productService: ProductService
-  ) { }
-
-  ngOnInit() {
-   this.products = this.productService.findAll();
+  ) {
   }
 
+  ngOnInit() {
+    this.products = this.productService.findAll();
+  }
 
+  add(id: string) {
+    var item: Item = {
+      product: this.productService.find(id),
+      quantity: 1
+    };
+    if (localStorage.getItem('cart') == null) {
+      let cart: any = [];
+      cart.push(JSON.stringify(item));
+      localStorage.setItem('cart', JSON.stringify(cart));
+    } else {
+      let cart: any = JSON.parse(localStorage.getItem('cart'));
+      let index: number = -1;
+      for (var i = 0; i < cart.length; i++) {
+        let item: Item = JSON.parse(cart[i]);
+        if (item.product.id == id) {
+          index = i;
+          break;
+        }
+      }
+      if (index == -1) {
+        cart.push(JSON.stringify(item));
+        localStorage.setItem('cart', JSON.stringify(cart));
+      } else {
+        let item: Item = JSON.parse(cart[index]);
+        item.quantity += 1;
+        cart[index] = JSON.stringify(item);
+        localStorage.setItem("cart", JSON.stringify(cart));
+      }
+    }
+  }
+  cartCount(): number {
+    this.total = 0;
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    for (var i = 0; i < cart.length; i++) {
+      let item = JSON.parse(cart[i]);
+      this.total += item.quantity;
+    }
+    return this.total;
+  }
 }
