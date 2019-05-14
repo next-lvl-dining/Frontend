93c9ca305689529a730a7a7b1ee34b15c1569528
Desktop-Jin <i305868@student.fontys.nl>
2019-05-14 15:30:12 +0200

    added shit

--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -3,20 +3,28 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {LocalOrder} from '../../models/localorder';
 import {catchError} from 'rxjs/operators';
+import {DeliveryOrder} from '../../models/deliveryorder';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class OrderService {
 
-  constructor(private http: HttpClient, @Inject('ORDER_API_URL') private ORDER_API_URL: string) { }
+    constructor(private http: HttpClient, @Inject('ORDER_API_URL') private ORDER_API_URL: string) {
+    }
+    getAllLocalOrderFromUser(userId: string): Observable<LocalOrder[]> {
+        console.log(this.ORDER_API_URL);
+        console.log(userId);
+        return this.http.get<LocalOrder[]>(this.ORDER_API_URL + '/localorders/all/' + userId)
+            .pipe(catchError(this.errorHandler));
+    }
 
-  getAllLocalOrderFromUser(userId: string): Observable<LocalOrder[]> {
-    return this.http.get<LocalOrder[]>(this.ORDER_API_URL + '/all' + '/' + userId)
-        .pipe(catchError(this.errorHandler));
-  }
+    getAllDeliveryOrderFromUser(userId: string): Observable<DeliveryOrder[]> {
+        return this.http.get<DeliveryOrder[]>(this.ORDER_API_URL + '/all/' + userId)
+            .pipe(catchError(this.errorHandler));
+    }
 
-  errorHandler(error: HttpErrorResponse) {
-    return throwError(error.error || 'Server error');
-  }
+    errorHandler(error: HttpErrorResponse) {
+        return throwError(error.error || 'Server error');
+    }
 }
