c5f0932a879f87d0b693d829ece433dd8b5af551
Unknown <ruudvdboomen@hotmail.com>
2019-06-04 13:41:16 +0200

    Add prometheus and grafana

--- src/app/models/portalrow.ts
+++ src/app/models/portalrow.ts
@@ -1,8 +1,8 @@
 export class Portalrow {
-   name: string;
-   url: string;
-   username: string;
-   password: string;
-   image: string;
-   resturl: string;
+  name: string;
+  url: string;
+  username: string;
+  password: string;
+  image: string;
+  resturl: string;
 }
