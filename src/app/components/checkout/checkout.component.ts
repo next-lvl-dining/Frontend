9df149b665f4965b0006680c78ffb0ab98abbbc7
rich <rd.richard@hotmail.com>
2019-06-05 23:28:47 +0200

    Added: credit card payment, button css all the same

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { PaymentService } from 'src/app/services/payment/payment.service';
+
 
 @Component({
   selector: 'app-checkout',
@@ -9,13 +11,12 @@ import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 export class CheckoutComponent implements OnInit {
   checkoutForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private paymentService: PaymentService) {
     this.createForm();
   }
 
   ngOnInit() {
   }
-
   createForm() {
     this.checkoutForm = this.formBuilder.group({
       address: ['', Validators.required],
@@ -25,5 +26,28 @@ export class CheckoutComponent implements OnInit {
       city: ['', Validators.required],
       phonenumber: ['', Validators.required]
     });
+  }
+
+  openCheckout() {
+    const handler = (window as any).StripeCheckout.configure({
+      // public test key to get stripe token
+      key: 'pk_test_huGgDHbJfir0H6xdkZ1JI9RJ00wQBU5ZhL',
+      locale: 'auto',
+      token: token => {
+        this.paymentService.creditCheckout(token.id, '100').subscribe(
+          data => {
+            // update order after that redirect to success delivery page
+          },
+          error => { console.log(error); });
+      }
+    });
+
+    handler.open({
+      name: 'Next Level Dining',
+      description: 'Food for all',
+      amount: 10000,
+      currency: 'EUR'
+    });
+
   }
 }
