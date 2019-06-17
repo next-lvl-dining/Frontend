720221e78454656818d3f2614303b181d33cc09e
zebybez <y.riesebos@student.fontys.nl>
2019-06-17 17:21:58 +0200

    added production environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+const baseUrl = '192.168.24.113';
+
 export const environment = {
-  production: true
+  production: true,
+
+  LOGGING_API_URL: `http://${baseUrl}:8082/logging/api`,
+  LOGIN_API_URL: `http://${baseUrl}:8083/login/api`,
+  PAYMENT_API_URL: `http://${baseUrl}:8085/payment/api`,
+  PROMOTION_API_URL: `http://${baseUrl}:8086/promotion/api`,
+  DELIVER_API_URL: `http://${baseUrl}:8088/deliver/api`,
+  RESERVE_API_URL: `http://${baseUrl}:8080/reserve/api`,
+  ORDER_API_URL: `http://${baseUrl}:8084/order/api`
 };
