87da65e4242b0f3308da60a7d8f605f1167049dc
Unknown <ruudvdboomen@hotmail.com>
2019-06-17 19:02:38 +0200

    Reservation error fix and uncomment category admin check

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,81 +1,80 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AuthService} from '../../services/auth/auth.service';
-import {CategoryService} from '../../services/category/category.service';
-import {Router} from '@angular/router';
-import {Category} from '../../models/category';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { CategoryService } from '../../services/category/category.service';
+import { Router } from '@angular/router';
+import { Category } from '../../models/category';
 
 @Component({
-    selector: 'app-category',
-    templateUrl: './category.component.html',
-    styleUrls: ['./category.component.scss']
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
 
-    categoryForm: FormGroup;
-    categories: Category[];
-    selectedOption: string;
+  categoryForm: FormGroup;
+  categories: Category[];
+  selectedOption: string;
 
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private authService: AuthService,
-        private categoryService: CategoryService,
-        private router: Router
-    ) {
-        this.categoryForm = this.formBuilder.group({
-            name: ['', Validators.required]
-        });
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private categoryService: CategoryService,
+    private router: Router
+  ) {
+    this.categoryForm = this.formBuilder.group({
+      name: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.checkIfAdmin();
+
+    this.categoryService.getAllCategories().subscribe(
+      data => {
+        this.categories = data;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+
+    console.log(this.selectedOption);
+  }
+
+  checkIfAdmin() {
+    if (!this.authService.hasRole('admin')) {
+      alert('Login as admin to continue');
+      this.router.navigateByUrl('/login');
     }
+  }
 
-    ngOnInit() {
-        //this.checkIfAdmin();
+  createCategory() {
+    if (this.categoryForm.invalid) {
+      alert('Please fill all fields correctly');
+    } else {
 
-        this.categoryService.getAllCategories().subscribe(
-            data => {
-                this.categories = data;
-                //console.log(data);
-            },
-            error => {
-                console.error(error);
-            }
-        );
+      const category: Category = {
+        name: this.categoryForm.value.name,
+      };
 
-        console.log(this.selectedOption);
+      console.log(category);
+
+      this.categoryService.createCategory(category).subscribe();
+      this.categoryForm.reset();
+      alert('Added new category');
     }
+  }
 
-    checkIfAdmin() {
-        if (!this.authService.hasRole('admin')) {
-            alert('Login as admin to continue');
-            this.router.navigateByUrl('/login');
-        }
+  deleteCategory() {
+    if (this.selectedOption != null) {
+      const category: Category = {
+        name: this.selectedOption,
+      };
+
+      console.log(category);
+      this.categoryService.deleteCategory(category);
     }
-
-    createCategory() {
-        if (this.categoryForm.invalid) {
-            alert('Please fill all fields correctly');
-        } else {
-
-            const category: Category = {
-                name: this.categoryForm.value.name,
-            };
-
-            console.log(category);
-
-            this.categoryService.createCategory(category).subscribe();
-            this.categoryForm.reset();
-            alert('Added new category');
-        }
-    }
-
-    deleteCategory() {
-        if (this.selectedOption != null) {
-            const category: Category = {
-                name: this.selectedOption,
-            };
-
-            console.log(category)
-            this.categoryService.deleteCategory(category);
-        }
-    }
+  }
 }
