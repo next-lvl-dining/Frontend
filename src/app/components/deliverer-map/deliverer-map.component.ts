784f7441c188c3cb1049c3a09d4fb929eaed3c8f
Unknown <ldamhuis@hotmail.com>
2019-06-17 18:26:41 +0200

    Moved array clear

--- src/app/components/deliverer-map/deliverer-map.component.ts
+++ src/app/components/deliverer-map/deliverer-map.component.ts
@@ -195,8 +195,8 @@ export class DelivererMapComponent implements OnInit {
         routes.push(route);
       }
       this.orderService.startDelivery(this.chosenOrders[i].id).subscribe();
-      this.chosenOrders = [];
     }
+    this.chosenOrders = [];
     empid = localStorage.getItem('id');
     var delivery = new Delivery(empid,orderids,routes);
     console.log("Delivery = " + delivery.routes[0].endLocation.latitude);
